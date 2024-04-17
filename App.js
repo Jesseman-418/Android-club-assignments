5b3be7326bf82043d69111be67ffdfa701055d1c
Jesseman-418 <143321248+Jesseman-418@users.noreply.github.com>
2024-04-17 20:02:25 +0530

    android club

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello, I'm Jesseman</Text>
+      <Button
+        title="Click Me"
+        onPress={() => alert('You clicked the button!')}
+      />
     </View>
   );
 }
@@ -13,8 +16,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#aaa', // Change the background color here
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 20,
   },
 });
